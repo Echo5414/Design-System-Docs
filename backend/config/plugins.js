b19fbe9de1e17d07a7e72366ade32f90eb9c8777
Echo5414 <sschiener@outlook.de>
2024-12-22 10:02:24 +0100

    Update for Strapi 5 changes

--- backend/config/plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = ({ env }) => ({
-  // Leave this empty or remove the GitHub configuration
-  // since it's already set in the admin UI
-}); 
--- backend/config/server.ts
+++ backend/config/server.ts
@@ -2,7 +2,16 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: env('URL', 'http://localhost:1337'),
+  proxy: env.bool('IS_PROXIED', true),
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', true),
   },
 });
